8ab3a761a372cb981ae7f238b09c17503fc12437
Mahiballah <145888808+mahibullah11@users.noreply.github.com>
2024-02-11 23:06:19 +0500

    add

---
+++ scirpt.js
@@ -0,0 +1,66 @@
+//TASK NO 1---------------------------------------------------------------------------->
+// let userInput = prompt("Enter a number:");
+// let number = parseInt(userInput);
+// let sum = 0;
+
+// while (!isNaN(number) && number > 0) {
+//     sum += number % 10;
+//     number = Math.floor(number / 10);
+// }
+
+// console.log("Sum of digits:", sum);
+
+
+//TASK NO 2----------------------------------------------------------------------->
+
+
+// let num1 = prompt("Enter the first number:");
+// let num2 = prompt("Enter the second number:");
+
+// num1 = parseInt(num1);
+// num2 = parseInt(num2);
+
+// let sum = 0;
+// for (let i = num1; i <= num2; i++) {
+//   sum += i;
+// }
+
+// alert(`The sum of the range from ${num1} to ${num2} is: ${sum}`);
+
+
+//TASK NO 3---------------------------------------------------------------------->
+
+
+// let num =  [84,75,58,68,57];
+// let sum = 0;
+// for(let value of num){
+//     sum += value;
+// }
+
+// let avg = sum / num.length;
+// console.log(`avg of the number = ${avg}`)
+
+
+// TASK NO 4----------------------------------------------------------------------->
+
+// let string1 = prompt("Enter the first string:");
+// let string2 = prompt("Enter the second string:");
+
+// if (string1 === string2) {
+//     console.log("characters are equal: " + string1);
+// } else {
+//     console.log("characters are not equal.");
+// }
+
+
+// TASK NO 5
+
+
+
+
+
+
+
+
+
+
